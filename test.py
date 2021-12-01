974a8a1367015952b28b60b880ee7aae6126b728
Yusif Osmanov <yusifosmanov475@gmail.com>
2021-12-02 01:45:29 +0400

    flask tutorial

--- test.py
+++ test.py
@@ -1,3 +1,36 @@
-mydict={'a':1,'b':2,'c':3,'e':5}
-dict_keys = mydict.keys()
-print(dict_keys)
+from flask import Flask, request
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return f"""<form action="/about" method='POST'>
+        <input type="text" placeholder="Insert log" name='name'>
+        <input type="text" placeholder="Insert password" name='pass'>
+        <input type="submit">
+    </form>
+    """
+
+
+@app.route('/about', methods=['GET','POST'])
+def about ():
+    if request.method=="POST":
+        ad=request.form['name']
+        parol=request.form['pass']
+    
+    return f"""
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <div class="card m-5" style="width: 18rem;">
+    <div class="card-body p-5">
+    <h5 class="card-title">{ad}</h5>
+    <p class="card-text">{parol}</p>
+    </div>
+    </div>
+    """   
+
+
+
+
+if __name__=='__main__':
+    app.run(debug=True)
