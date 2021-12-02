ad10deef7c779f67e6345c35a629d8691dc329f7
Yusif Osmanov <yusifosmanov475@gmail.com>
2021-12-02 21:32:39 +0400

    flask test

--- test.py
+++ test.py
@@ -4,27 +4,27 @@ app=Flask(__name__)
 
 @app.route('/')
 def index():
-    return f"""<form action="/about" method='POST'>
-        <input type="text" placeholder="Insert log" name='name'>
-        <input type="text" placeholder="Insert password" name='pass'>
+    return f"""<form action="/news" method='POST'>
+        <input type="text" placeholder="Insert log" name='title'>
+        <input type="text" placeholder="Insert password" name='content'>
         <input type="submit">
     </form>
     """
 
 
-@app.route('/about', methods=['GET','POST'])
-def about ():
+@app.route('/news', methods=['GET','POST'])
+def news():
     if request.method=="POST":
-        ad=request.form['name']
-        parol=request.form['pass']
+        title=request.form['title']
+        content=request.form['content']
     
     return f"""
 <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <div class="card m-5" style="width: 18rem;">
     <div class="card-body p-5">
-    <h5 class="card-title">{ad}</h5>
-    <p class="card-text">{parol}</p>
+    <h5 class="card-title">{title}</h5>
+    <p class="card-text">{content}</p>
     </div>
     </div>
     """   
